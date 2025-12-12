cfc3fadc0513d4dedc0c1b756a877c1fad882715
Doomlead <em3plum@gmail.com>
2025-12-11 22:14:33 -0500

    Add procedural parallax background pipeline

---
+++ js/graphics/parallaxManager.js
@@ -0,0 +1,80 @@
+// Manages TileSprite layers and seamless scrolling using accumulated offsets
+
+class ParallaxManager {
+    constructor(scene, config) {
+        this.scene = scene;
+        this.config = config;
+        this.layers = [];
+        this._prevPlayerX = 0;
+        this._accumScrollX = 0;
+    }
+
+    createLayers() {
+        const { worldWidth, worldHeight } = this.config;
+        const camWidth = this.config.width;
+        const camHeight = this.config.height;
+
+        for (const layerName of LAYER_ORDER) {
+            const layerConfig = BACKGROUND_LAYERS[layerName];
+
+            if (!this.scene.textures.exists(layerConfig.key)) {
+                continue;
+            }
+
+            const texture = this.scene.textures.get(layerConfig.key);
+            const frame = texture.getSourceImage();
+            const texHeight = frame.height;
+
+            const tileSprite = this.scene.add.tileSprite(0, 0, camWidth, camHeight, layerConfig.key);
+
+            tileSprite.setOrigin(0, 0);
+            tileSprite.setScrollFactor(0);
+            tileSprite.setDepth(layerConfig.depth);
+
+            const scaleY = camHeight / texHeight;
+            tileSprite.setScale(1, scaleY);
+
+            this.layers.push({
+                sprite: tileSprite,
+                speedX: layerConfig.speedX,
+                name: layerName,
+            });
+        }
+    }
+
+    initTracking(playerX) {
+        this._prevPlayerX = playerX;
+        this._accumScrollX = 0;
+    }
+
+    update(playerX) {
+        const worldWidth = this.config.worldWidth;
+        let dx = playerX - this._prevPlayerX;
+        const halfWorld = worldWidth * 0.5;
+        if (dx > halfWorld) {
+            dx -= worldWidth;
+        } else if (dx < -halfWorld) {
+            dx += worldWidth;
+        }
+
+        this._accumScrollX += dx;
+        this._prevPlayerX = playerX;
+
+        for (const layer of this.layers) {
+            layer.sprite.tilePositionX = this._accumScrollX * layer.speedX;
+        }
+    }
+
+    refresh() {
+        for (const layer of this.layers) {
+            layer.sprite.tilePositionX = this._accumScrollX * layer.speedX;
+        }
+    }
+
+    destroy() {
+        for (const layer of this.layers) {
+            layer.sprite.destroy();
+        }
+        this.layers = [];
+    }
+}
